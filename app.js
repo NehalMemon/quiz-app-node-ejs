4540492d208b76a702b7ed87bb965ad011a39e45
Nehal <nehalmemon73@gmail.com>
2025-07-24 20:08:56 +0500

    add module system in quiz

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(coookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,"src")))
+app.set('views', path.join(__dirname, 'views')); 
 app.use(express.static(path.join(__dirname,"public")))
 app.use(session({
   secret: process.env.EXPRESS_SESSION_SECRET,
